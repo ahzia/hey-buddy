fcc8c0101a2e010ae3f5040623a55624a4cee8da
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2024-06-03 21:50:06 +0200

    solved the linters single quotes issues

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "settings",
-    title: "Open Settings",
-    contexts: ["action"]
+    id: 'settings',
+    title: 'Open Settings',
+    contexts: ['action']
   });
 
   chrome.contextMenus.onClicked.addListener((info) => {
-    if (info.menuItemId === "settings") {
+    if (info.menuItemId === 'settings') {
       chrome.runtime.openOptionsPage();
     }
   });
